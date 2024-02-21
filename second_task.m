6b2d686c5b07740a3e7e8cf9b44d9a133f4ffc10
maksimpugacev <pugachev_maxim@inbox.ru>
2024-02-21 11:49:32 +0300

    upd

--- second_task.m
+++ second_task.m
@@ -1,96 +1,129 @@
-% Загрузка данных
 load('calib.mat');
 Q = calib.Q;
-T_actual = calib.T;
+T_real = calib.T;
 
-% Начальное приближение параметров
-initial_params = [300, 0, pi/2, 0, 400, 0, 0, 500, 0, 0, 0, 0];
+%init_par =[d1, a1, alpha1, 
+                % d2, a2, alpha2,
+                % d3, a3, alpha3,
+                % theta1, theta2, theta3]
+                  
+init_par = [300, 0, pi/2, ...
+                  0, 400, 0, ...
+                  0, 500, 0, ...
+                  0, 0, 0];
+              
+%boundaries: angle +- 0.01rad, lenght +- 10mm
+% lb = [290, -0.01, pi/2-0.01, -10, 390, -0.01, -10, 490, -0.01, -0.01, -0.01, -0.01];
+% ub = [310, 0.01, pi/2+0.01, 10, 410, 0.01, 10, 510, 0.01, 0.01, 0.01, 0.01];
 
-% Ограничения для параметров
-lb = [290, -0.01, pi/2-0.01, -10, 390, -0.01, -10, 490, -0.01, -10, -0.01, -0.01];
-ub = [310, 0.01, pi/2+0.01, 10, 410, 0.01, 10, 510, 0.01, 10, 0.01, 0.01];
+lb = [290, -10, pi/2 - 0.01, -10 , 390, -0.01, -10, 490, -0.01, -0.01, -0.01, -0.01];
+ub = [310, 10, pi/2 + 0.01, 10 , 410, 0.01, 10, 510, 0.01, 0.01, 0.01 , 0.01];
 
-% Опции для fmincon
+%options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 options = optimset('TolX',1e-16,'MaxIter',2e3,'MaxFunEvals',2000);
+[optimized_params, fval] = fmincon(@(params) obj_func(params, Q, T_real),init_par,[], [], [], [], lb, ub, [], options);
 
-% Выполнение оптимизации
-[optimized_params, fval] = fmincon(@(params) objective_function(params, Q, T_actual), initial_params, [], [], [], [], lb, ub, [], options);
-
-% Вывод оптимизированных параметров
-disp('Оптимизированные параметры:');
+disp('Optimal parameters:');
 disp(optimized_params);
+disp('mean before:');
+disp(obj_func(init_par, Q, T_real));
+disp('mean after:');
+disp(obj_func(optimized_params, Q, T_real));
 
-function [d, R] = manipulator_model(q, params)
-    % Извлечение параметров для каждого звена
+
+%Параметры DH (по паспорту):
+%_____________________
+% theta1 = 0.2268;
+% theta2 = 2.3512;
+% theta3 = 2.2005;
+% 
+% d1 = 300;
+% d2 = 0;
+% d3 = 0;
+% 
+% alpha1 = pi/2;
+% alpha2 = 0;
+% alpha3 = 0;
+% 
+% a1 = 0;
+% a2 = 400;
+% a3 = 500;
+%_____________________
+%Прямая кинематики:
+% T_kinematic = T_dh(theta1, d1, alpha1, a1) *  ... 
+%     T_dh(theta2, d2, alpha2, a2) * ...
+%     T_dh(theta3, d3, alpha3, a3);
+% 
+% d = T_kinematic(1:3, 4);
+% R = T_kinematic(1:3, 1:3);
+
+%----------------------------------------------------------------%
+function total_diff = obj_func(params, Q, T_real)
+    diff = 0;
+    total_diff = 0;
     d1 = params(1);
     a1 = params(2);
     alpha1 = params(3);
-
+    theta1 = params(10);
+    
     d2 = params(4);
     a2 = params(5);
     alpha2 = params(6);
+    theta2 = params(11);
 
     d3 = params(7);
     a3 = params(8);
     alpha3 = params(9);
-
-    % Передача углов и параметров в DH преобразование
-    T1 = dh_transform(q(1), d1, a1, alpha1);
-    T2 = dh_transform(q(2), d2, a2, alpha2);
-    T3 = dh_transform(q(3), d3, a3, alpha3);
-
-    % Общая матрица преобразования
-    T = T1 * T2 * T3;
-
-    d = T(1:3, 4);
-    R = T(1:3, 1:3);
-end
-
-function total_diff = objective_function(params, Q, T_actual)
-    total_diff = 0;
+    theta3 = params(12);
+    
     for i = 1:size(Q, 1)
-        [d, R] = manipulator_model(Q(i, :), params);
-        T_calculated = [R d; 0 0 0 1];
-
-        % Вычисление нормы Фробениуса разности между матрицами
-        diff = norm(T_calculated - T_actual(:, :, i), 'fro');
+        T_kinematic = T_dh(Q(i, 1) + theta1, d1, alpha1, a1) *  ... 
+                      T_dh(Q(i, 2) + theta2, d2, alpha2, a2) * ...
+                      T_dh(Q(i, 3) + theta3, d3, alpha3, a3);
+        %d = T_kinematic(1:3, 4);
+        %R = T_kinematic(1:3, 1:3);
+        
+                  
+%         diff = norm(T_kinematic - T_real(:, :, i), 'fro')
+        
+        diff = norm(T_kinematic(1:3,4) - T_real(1:3, 4, i),2)^2;
         total_diff = total_diff + diff;
     end
+    
+    
 end
-
-function T = dh_transform(theta, d, a, alpha)
-    % T = [cos(theta), sin(theta)*cos(alpha), sin(theta)*sin(alpha), a*cos(theta);
-    %      sin(theta), cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
-    %      0, sin(alpha), cos(alpha), d;
-    %      0, 0, 0, 1];
-    T = rotz(theta)*transz(d)*rotx(alpha)*transx(a);
-
-end
-
-function Rz = rotz(theta)
+%R_z(theta) - матрица поворота на угол theta отн-но оси Z
+%T_z(d) - матрица смещения на величину d вдоль оси Z
+function Rz = rot_z(theta) 
     Rz = [cos(theta) -sin(theta) 0 0;
           sin(theta) cos(theta) 0 0;
           0 0 1 0;
           0 0 0 1];
 end
 
-function Tz = transz(d)
+function Tz = trans_z(d)
     Tz = [1 0 0 0;
           0 1 0 0;
           0 0 1 d;
           0 0 0 1];
 end
 
-function Rx = rotx(alpha)
+function Rx = rot_x(alpha)
     Rx = [1 0 0 0;
           0 cos(alpha) -sin(alpha) 0;
           0 sin(alpha) cos(alpha) 0;
           0 0 0 1];
 end
 
-function Tx = transx(a)
+function Tx = trans_x(a)
     Tx = [1 0 0 a;
           0 1 0 0;
           0 0 1 0;
           0 0 0 1];
 end
+
+%Матрциа  представления Денавита-Хартенберга (T_dh)
+function T = T_dh(theta, d, alpha, a)
+    T = rot_z(theta)*trans_z(d)*rot_x(alpha)*trans_x(a);
+end
+
